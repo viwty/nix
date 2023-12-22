026a166068639b129a829b143f3e693836b4add2
viwty <viwty@proton.me>
2023-12-22 22:14:51 +0300

    this made it build, why? i do not know

--- modules/home-manager/mako/default.nix
+++ modules/home-manager/mako/default.nix
@@ -8,7 +8,6 @@ in {
     backgroundColor = "#${colors.base00}";
     borderColor = "#${colors.base0D}";
     textColor = "#${colors.base05}";
-    font = "${config.font} 12";
 
     layer = "overlay";
     defaultTimeout = 5000;
